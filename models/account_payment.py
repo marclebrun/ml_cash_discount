11624e81bf1a4245e442c5f60453cefa627f9f17
Marc Lebrun <marclebrun2@gmail.com>
2020-07-27 14:30:59 +0200

    Display cash discount information on payment form

--- models/account_payment.py
+++ models/account_payment.py
@@ -6,11 +6,32 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     bigoudi = fields.Char(string="Bigoudi", default="Trololol")
+    infos_cd = fields.Html(string="Cash Discount information")
 
     @api.multi
     def _compute_payment_amount(self, invoices=None, currency=None):
         total = super(AccountPayment, self)._compute_payment_amount(invoices, currency)
-        total *= 2
+
+        if not invoices:
+            invoices = self.invoice_ids
+
+        infos = ""
+
+        for inv in invoices:
+            discount_applied = inv.date_cd.today() <= inv.date_cd
+
+            infos += "<table>"
+            infos += "<tr><td>Invoice            </td><td><strong>%s  </strong></td></tr>" % inv.number
+            infos += "<tr><td>Total to pay       </td><td><strong>%.2f</strong></td></tr>" % inv.residual_signed
+            infos += "<tr><td>Total with discount</td><td><strong>%.2f</strong></td></tr>" % inv.total_cd
+            infos += "<tr><td>Discount validity  </td><td><strong>%s  </strong></td></tr>" % inv.date_cd
+            infos += "<tr><td>Discount is applied</td><td><strong>%s  </strong></td></tr>" % ("Yes" if discount_applied else "No")
+            infos += "</table>"
+
+            if discount_applied:
+                total = inv.total_cd
+
         for rec in self:
-            rec.bigoudi = "Calcul effectué"
+            rec.infos_cd = infos
+        
         return total
