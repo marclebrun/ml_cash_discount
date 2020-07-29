ec9872b979f86001f8bb111bc62b1670b344edab
Marc Lebrun <marclebrun2@gmail.com>
2020-07-29 22:35:11 +0200

    Recomputes payment amount when payment date is changed

--- models/account_payment.py
+++ models/account_payment.py
@@ -5,11 +5,15 @@ from odoo import api, fields, models
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    bigoudi = fields.Char(string="Bigoudi", default="Trololol")
     infos_cd = fields.Html(string="Cash Discount information")
 
     @api.multi
     def _compute_payment_amount(self, invoices=None, currency=None):
+        """
+        If the date of payment is less or equal to the Cash Discount validity date,
+        then the total amount of the payment is replaced with the discounted amount
+        of the invoice.
+        """
         total = super(AccountPayment, self)._compute_payment_amount(invoices, currency)
 
         if not invoices:
@@ -18,13 +22,14 @@ class AccountPayment(models.Model):
         infos = ""
 
         for inv in invoices:
-            discount_applied = inv.date_cd.today() <= inv.date_cd
+            discount_applied = self.payment_date and (self.payment_date <= inv.date_cd)
 
             infos += "<table>"
             infos += "<tr><td>Invoice            </td><td><strong>%s  </strong></td></tr>" % inv.number
             infos += "<tr><td>Total to pay       </td><td><strong>%.2f</strong></td></tr>" % inv.residual_signed
             infos += "<tr><td>Total with discount</td><td><strong>%.2f</strong></td></tr>" % inv.total_cd
             infos += "<tr><td>Discount validity  </td><td><strong>%s  </strong></td></tr>" % inv.date_cd
+            infos += "<tr><td>Payment date       </td><td><strong>%s  </strong></td></tr>" % self.payment_date
             infos += "<tr><td>Discount is applied</td><td><strong>%s  </strong></td></tr>" % ("Yes" if discount_applied else "No")
             infos += "</table>"
 
@@ -35,3 +40,12 @@ class AccountPayment(models.Model):
             rec.infos_cd = infos
         
         return total
+
+    @api.onchange('payment_date')
+    def _onchange_payment_date(self):
+        #res = super(account_register_payments, self)._onchange_journal()
+        #active_ids = self._context.get('active_ids')
+        #invoices = self.env['account.invoice'].browse(active_ids)
+        #self.amount = abs(self._compute_payment_amount(invoices))
+        #return res
+        self.amount = self._compute_payment_amount()
