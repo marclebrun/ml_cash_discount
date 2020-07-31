060faa014036bba0a81013d0c1d01229588ebb22
Marc Lebrun <marclebrun2@gmail.com>
2020-07-31 09:55:07 +0200

    Added discount information

--- models/account_payment.py
+++ models/account_payment.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
@@ -24,13 +24,19 @@ class AccountPayment(models.Model):
         for inv in invoices:
             discount_applied = self.payment_date and (self.payment_date <= inv.date_cd)
 
+            if inv.payment_term_id and inv.payment_term_id.cd_percent:
+                validity = "(%d %s)" % (inv.payment_term_id.cd_delay, _("days"))
+            else:
+                validity = ""
+
             infos += "<table>"
-            infos += "<tr><td>Invoice            </td><td><strong>%s  </strong></td></tr>" % inv.number
-            infos += "<tr><td>Total to pay       </td><td><strong>%.2f</strong></td></tr>" % inv.residual_signed
-            infos += "<tr><td>Total with discount</td><td><strong>%.2f</strong></td></tr>" % inv.total_cd
-            infos += "<tr><td>Discount validity  </td><td><strong>%s  </strong></td></tr>" % inv.date_cd
-            infos += "<tr><td>Payment date       </td><td><strong>%s  </strong></td></tr>" % self.payment_date
-            infos += "<tr><td>Discount is applied</td><td><strong>%s  </strong></td></tr>" % ("Yes" if discount_applied else "No")
+            infos += "<tr><td>" + _("Invoice number")      + "</td><td><strong>%s  </strong></td></tr>" % inv.number
+            infos += "<tr><td>" + _("Date of invoice")     + "</td><td><strong>%s  </strong></td></tr>" % inv.date_invoice
+            infos += "<tr><td>" + _("Discount validity")   + "</td><td><strong>%s %s</strong></td></tr>" % (inv.date_cd, validity)
+            infos += "<tr><td>" + _("Payment date")        + "</td><td><strong>%s  </strong></td></tr>" % self.payment_date
+            infos += "<tr><td>" + _("Total to pay")        + "</td><td><strong>%.2f</strong></td></tr>" % inv.residual_signed
+            infos += "<tr><td>" + _("Total with discount") + "</td><td><strong>%.2f</strong></td></tr>" % inv.total_cd
+            infos += "<tr><td>" + _("Discount is applied") + "</td><td><strong>%s  </strong></td></tr>" % (_("Yes") if discount_applied else _("No"))
             infos += "</table>"
 
             if discount_applied:
