c73e9301a99e6eb1b363682a84bbc65c20abae82
Marc Lebrun <marclebrun2@gmail.com>
2020-08-01 10:09:16 +0200

    Solved bug with invoices without cash discount

--- models/account_payment.py
+++ models/account_payment.py
@@ -15,6 +15,7 @@ class AccountPayment(models.Model):
         of the invoice.
         """
         total = super(AccountPayment, self)._compute_payment_amount(invoices, currency)
+        # difference = 0.0
 
         if not invoices:
             invoices = self.invoice_ids
@@ -22,36 +23,42 @@ class AccountPayment(models.Model):
         infos = ""
 
         for inv in invoices:
-            discount_applied = self.payment_date and (self.payment_date <= inv.date_cd)
 
-            if inv.payment_term_id and inv.payment_term_id.cd_percent:
-                validity = "(%d %s)" % (inv.payment_term_id.cd_delay, _("days"))
-            else:
-                validity = ""
+            discount_applied = False
+            
+            if inv.show_cd:
+                discount_applied = self.payment_date and (self.payment_date <= inv.date_cd)
 
-            infos += "<table>"
-            infos += "<tr><td>" + _("Invoice number")      + "</td><td><strong>%s  </strong></td></tr>" % inv.number
-            infos += "<tr><td>" + _("Date of invoice")     + "</td><td><strong>%s  </strong></td></tr>" % inv.date_invoice
-            infos += "<tr><td>" + _("Discount validity")   + "</td><td><strong>%s %s</strong></td></tr>" % (inv.date_cd, validity)
-            infos += "<tr><td>" + _("Payment date")        + "</td><td><strong>%s  </strong></td></tr>" % self.payment_date
-            infos += "<tr><td>" + _("Total to pay")        + "</td><td><strong>%.2f</strong></td></tr>" % inv.residual_signed
-            infos += "<tr><td>" + _("Total with discount") + "</td><td><strong>%.2f</strong></td></tr>" % inv.total_cd
-            infos += "<tr><td>" + _("Discount is applied") + "</td><td><strong>%s  </strong></td></tr>" % (_("Yes") if discount_applied else _("No"))
-            infos += "</table>"
+                # if discount_applied:
+                #     difference += (inv.residual_signed - inv.total_cd)
 
-            if discount_applied:
-                total = inv.total_cd
+                if inv.payment_term_id and inv.payment_term_id.cd_percent:
+                    validity = "(%d %s)" % (inv.payment_term_id.cd_delay, _("days"))
+                else:
+                    validity = ""
+
+                infos += "<table>"
+                infos += "<tr><td>" + _("Invoice number")      + "</td><td><strong>%s  </strong></td></tr>" % inv.number
+                infos += "<tr><td>" + _("Date of invoice")     + "</td><td><strong>%s  </strong></td></tr>" % inv.date_invoice
+                infos += "<tr><td>" + _("Discount validity")   + "</td><td><strong>%s %s</strong></td></tr>" % (inv.date_cd, validity)
+                infos += "<tr><td>" + _("Payment date")        + "</td><td><strong>%s  </strong></td></tr>" % self.payment_date
+                infos += "<tr><td>" + _("Total to pay")        + "</td><td><strong>%.2f</strong></td></tr>" % inv.residual_signed
+                infos += "<tr><td>" + _("Total with discount") + "</td><td><strong>%.2f</strong></td></tr>" % inv.total_cd
+                infos += "<tr><td>" + _("Discount is applied") + "</td><td><strong>%s  </strong></td></tr>" % (_("Yes") if discount_applied else _("No"))
+                infos += "</table>"
+
+                if discount_applied:
+                    total = inv.total_cd
 
         for rec in self:
             rec.infos_cd = infos
+            # rec.payment_difference = difference
+            # rec.payment_difference_handling = 'reconcile'
+            # rec.writeoff_account_id = inv.company_id.out_inv_cd_account_id
+            # rec.writeoff_label = 'Bigoudi'
         
         return total
 
     @api.onchange('payment_date')
     def _onchange_payment_date(self):
-        #res = super(account_register_payments, self)._onchange_journal()
-        #active_ids = self._context.get('active_ids')
-        #invoices = self.env['account.invoice'].browse(active_ids)
-        #self.amount = abs(self._compute_payment_amount(invoices))
-        #return res
         self.amount = self._compute_payment_amount()
